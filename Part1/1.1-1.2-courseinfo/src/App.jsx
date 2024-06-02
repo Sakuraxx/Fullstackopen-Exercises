f3fe129293dabd1bc6b9afabba07f3d7b286442a
Jingwen Xia <amyfor7@gmail.com>
2024-06-02 13:44:08 +0800

    refactor: use js object

--- Part1/1.1-1.2-courseinfo/src/App.jsx
+++ Part1/1.1-1.2-courseinfo/src/App.jsx
@@ -4,9 +4,10 @@ const Header = (props) => {
   )
 }
 
-const Total = (props) => {
+const Total = ({parts}) => {
+  const totalExs = parts.reduce((sum, part) => sum + part.exercises, 0);
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {totalExs}</p>
   )
 }
 
@@ -28,27 +29,29 @@ const Content = ({ parts }) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
-
-  const parts = [part1, part2, part3];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
