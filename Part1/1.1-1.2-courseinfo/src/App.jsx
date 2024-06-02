791fb077be4d8a1dded14a24dc75289e8f79ab3d
Jingwen Xia <amyfor7@gmail.com>
2024-06-02 13:38:22 +0800

    refactor: use js object

--- Part1/1.1-1.2-courseinfo/src/App.jsx
+++ Part1/1.1-1.2-courseinfo/src/App.jsx
@@ -22,30 +22,33 @@ const Part = ({ part, exercise }) => {
 const Content = ({ parts }) => {
   return (
     <>
-      {parts.map((part, index) => (<Part key={index} part={part.part} exercise={part.exercise} />))}
+      {parts.map((part, index) => (<Part key={index} part={part.name} exercise={part.exercises} />))}
     </>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development';
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  };
 
-  const parts = [
-    { part: part1, exercise: exercises1 },
-    { part: part2, exercise: exercises2 },
-    { part: part3, exercise: exercises3 }]
+  const parts = [part1, part2, part3];
 
   return (
     <div>
       <Header course={course} />
       <Content parts={parts} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
