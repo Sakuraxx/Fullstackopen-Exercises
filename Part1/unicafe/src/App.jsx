d97d65b0e54d153828d41aa6a56c30477bf46ac4
Jingwen Xia <amyfor7@gmail.com>
2024-06-02 16:19:17 +0800

    feat: add average and positive statistics

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 
-const Button = ({text, value, onClick}) => {
+const Button = ({ text, value, onClick }) => {
   console.log(text, value);
   return <button onClick={() => onClick(value + 1)}>{text}</button>
 }
 
 
 function App() {
-   // save clicks of each button to its own state
-   const [good, setGood] = useState(0)
-   const [neutral, setNeutral] = useState(0)
-   const [bad, setBad] = useState(0)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const total = good + neutral + bad;
+  const average = total == 0 ? 0 : good - bad / total;
+  const positive = total == 0 ? 0 : good / total * 100;
 
   return (
     <>
@@ -22,6 +26,9 @@ function App() {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {total}</p>
+      <p>average score: {average}</p>
+      <p>positive: {positive}%</p>
     </>
   )
 }
