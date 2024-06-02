8002e8d5d137e116d32f840b6b0eec5c4772a500
Jingwen Xia <amyfor7@gmail.com>
2024-06-02 16:22:50 +0800

    refactor: extract Statistics sub component

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -5,6 +5,24 @@ const Button = ({ text, value, onClick }) => {
   return <button onClick={() => onClick(value + 1)}>{text}</button>
 }
 
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad;
+  const average = total == 0 ? 0 : good - bad / total;
+  const positive = total == 0 ? 0 : good / total * 100;
+
+  return (
+    <>
+        <h1>statistics</h1>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+        <p>all: {total}</p>
+        <p>average score: {average}</p>
+        <p>positive: {positive}%</p>
+    </>
+
+  )
+}
 
 function App() {
   // save clicks of each button to its own state
@@ -12,23 +30,13 @@ function App() {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good + neutral + bad;
-  const average = total == 0 ? 0 : good - bad / total;
-  const positive = total == 0 ? 0 : good / total * 100;
-
   return (
     <>
       <h1>give feedback</h1>
       <Button text="good" value={good} onClick={setGood} ></Button>
       <Button text="neutral" value={neutral} onClick={setNeutral} ></Button>
       <Button text="bad" value={bad} onClick={setBad} ></Button>
-      <h1>statistics</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {total}</p>
-      <p>average score: {average}</p>
-      <p>positive: {positive}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </>
   )
 }
