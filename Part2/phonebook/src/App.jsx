f50a5158260d18d8ae4e3e04e1d905447e632b21
Jingwen Xia <amyfor7@gmail.com>
2024-06-10 20:01:13 +0800

    feat: add phone number field

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   function checkIfExistSameNameInContact(name) {
     for (const person of persons) {
@@ -22,15 +25,20 @@ const App = () => {
 
     const newPerson = {
       name: newName,
+      number: newNumber,
     };
     const newPersons = [...persons, newPerson];
     setPersons(newPersons);
     setNewName("");
+    setNewNumber("");
   }
 
-  function handleOnChange(event) {
-    console.log("onchange", event.target.value);
+  function handleOnChangeName(event) {
     setNewName(event.target.value);
+  }
+
+  function handleOnChangeNumber(event) {
+    setNewNumber(event.target.value);
   }
 
   return (
@@ -38,7 +46,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleOnChange} />
+          name: <input value={newName} onChange={handleOnChangeName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleOnChangeNumber} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -47,7 +58,9 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((p, key) => (
-          <li key={key}>{p.name}</li>
+          <li key={key}>
+            {p.name} {p.number}
+          </li>
         ))}
       </ul>
     </div>
