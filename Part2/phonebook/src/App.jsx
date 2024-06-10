0ac49b0f73f782a8a8fde9f0dc4db585b643bd7c
Jingwen Xia <amyfor7@gmail.com>
2024-06-10 20:14:38 +0800

    feat: add filter

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -2,10 +2,19 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterName, setFilterName] = useState("");
+
+  const shownPersons =
+    filterName === ""
+      ? persons
+      : persons.filter((p) => p.name.includes(filterName));
 
   function checkIfExistSameNameInContact(name) {
     for (const person of persons) {
@@ -41,9 +50,16 @@ const App = () => {
     setNewNumber(event.target.value);
   }
 
+  function handleOnChangeFilter(event) {
+    setFilterName(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with{" "}
+      <input value={filterName} onChange={handleOnChangeFilter} />
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleOnChangeName} />
@@ -57,8 +73,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((p, key) => (
-          <li key={key}>
+        {shownPersons.map((p, id) => (
+          <li key={id}>
             {p.name} {p.number}
           </li>
         ))}
