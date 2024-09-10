86d06705cbbae88a8469263fdbfdf62d841eee69
Jingwen Xia <amyfor7@gmail.com>
2024-09-10 20:45:20 +0800

    feat: add user router

--- Part4/app.js
+++ Part4/app.js
@@ -5,12 +5,15 @@ const blogsRouter = require("./controllers/blogs");
 const middleware = require("./utils/middleware");
 const app = express();
 
+const usersRouter = require('./controllers/users');
+
 app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- Part4/tests/test_helper.js
+++ Part4/tests/test_helper.js
@@ -1,4 +1,5 @@
-const Blog = require('../models/blog')
+const Blog = require('../models/blog');
+const User = require('../models/user');
 
 const initialBlogs = [
     {
@@ -16,11 +17,17 @@ const initialBlogs = [
 ];
 
 const blogsInDb = async () => {
-    const blogs = await Blog.find({})
-    return blogs.map(blog => blog.toJSON())
+    const blogs = await Blog.find({});
+    return blogs.map(blog => blog.toJSON());
+}
+
+const usersInDb = async () => {
+    const users = await User.find({});
+    return users.map(user => user.toJSON());
 }
 
 module.exports = {
     initialBlogs,
-    blogsInDb
+    blogsInDb,
+    usersInDb
 }
