1cc262f05761ea0838991cc8d3852f79e9a0d516
Jingwen Xia <amyfor7@gmail.com>
2024-09-10 21:07:06 +0800

    feat: both username and password must be given and both must be at least 3 characters long

--- Part4/controllers/users.js
+++ Part4/controllers/users.js
@@ -10,6 +10,22 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body;
 
+    if (username === undefined) {
+        return response.status(400).json({ error: 'expected `username` to be provided' });
+    }
+
+    if (username.length < 3) {
+        return response.status(400).json({ error: 'expected `username` to be at be at least 3 characters long' });
+    }
+
+    if (password === undefined) {
+        return response.status(400).json({ error: 'expected `password` to be provided' });
+    }
+
+    if (password.length < 3) {
+        return response.status(400).json({ error: 'expected `password` to be at be at least 3 characters long' });
+    }
+
     const saltRounds = 7;
     const passwordHash = await bcrypt.hash(password, saltRounds);
 
--- Part4/tests/user_api.test.js
+++ Part4/tests/user_api.test.js
@@ -64,6 +64,87 @@ describe('when there is initially one user in db', () => {
 
 });
 
+
+describe('when user added is invalid', () => {
+    test('username is not provided', async () => {
+        const usersAtStart = await usersInDb();
+
+        const newUser = {
+            name: 'Little Bread',
+            password: 'sakuraxx',
+        };
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/);
+
+        const usersAtEnd = await usersInDb();
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length);
+        assert(result.body.error.includes('expected `username` to be provided'));
+    });
+
+    test('the length of username is less than 3 characters', async () => {
+        const usersAtStart = await usersInDb();
+
+        const newUser = {
+            username: 'bb',
+            name: 'Little Bread',
+            password: 'sakuraxx',
+        };
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/);
+
+        const usersAtEnd = await usersInDb();
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length);
+        assert(result.body.error.includes('expected `username` to be at be at least 3 characters long'));
+    });
+
+    test('password is not provided', async () => {
+        const usersAtStart = await usersInDb();
+
+        const newUser = {
+            username: 'LittleBread',
+            name: 'Little Bread',
+        };
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/);
+
+        const usersAtEnd = await usersInDb();
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length);
+        assert(result.body.error.includes('expected `password` to be provided'));
+    });
+
+    test('the length of password is less than 3 characters', async () => {
+        const usersAtStart = await usersInDb();
+
+        const newUser = {
+            username: 'bbbb',
+            name: 'Little Bread',
+            password: 'sa',
+        };
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/);
+
+        const usersAtEnd = await usersInDb();
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length);
+        assert(result.body.error.includes('expected `password` to be at be at least 3 characters long'));
+    });
+});
+
 after(async () => {
     await mongoose.connection.close();
 });
