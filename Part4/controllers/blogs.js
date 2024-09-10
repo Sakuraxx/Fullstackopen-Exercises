b0496fc98ebd4280c7f1075779f755b7149eba7f
Jingwen Xia <amyfor7@gmail.com>
2024-09-10 21:31:14 +0800

    feat:each blog contains information on the creator of the blog

--- Part4/controllers/blogs.js
+++ Part4/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require('../models/user');
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user');
   response.json(blogs);
 });
 
@@ -20,11 +21,22 @@ blogsRouter.post("/", async (request, response) => {
   if (!body.title || !body.url) {
     return response.status(400).json({ error: 'title or url is missing' });
   }
+
+  if (!body.userId) {
+    return response.status(400).json({ error: 'user id is missing' });
+  }
+
   if (body.likes === undefined) {
     body.likes = 0;
   }
-  const blog = new Blog(body);
+
+  const user = await User.findById(body.userId)
+
+  const blog = new Blog({ ...body, user: user.id });
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
   response.status(201).json(savedBlog);
 });
 
