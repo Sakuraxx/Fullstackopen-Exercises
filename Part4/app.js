b63726a71c47734b2ff3b6bbc6ae3e79e015fd46
Jingwen Xia <amyfor7@gmail.com>
2024-09-11 22:57:10 +0800

    feat: generate token

--- Part4/app.js
+++ Part4/app.js
@@ -7,6 +7,8 @@ const app = express();
 
 const usersRouter = require('./controllers/users');
 
+const loginRouter = require('./controllers/login');
+
 app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
@@ -14,6 +16,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
