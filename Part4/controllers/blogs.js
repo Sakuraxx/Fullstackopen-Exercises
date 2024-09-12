bec2562cf5173eb57a0446b9d2d730378a73bbef
Jingwen Xia <amyfor7@gmail.com>
2024-09-12 22:11:43 +0800

    refactor: extract the method taking the token to the middleware

--- Part4/controllers/blogs.js
+++ Part4/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require("../models/blog");
 const User = require('../models/user');
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate('user');
   response.json(blogs);
@@ -40,7 +32,7 @@ blogsRouter.post("/", async (request, response) => {
     body.likes = 0;
   }
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' });
   }
--- Part4/controllers/login.js
+++ Part4/controllers/login.js
@@ -24,7 +24,7 @@ loginRouter.post('/', async (req, resp) => {
     const token = jwt.sign(
         userForToken,
         process.env.SECRET,
-        { expiresIn: 60 }
+        { expiresIn: 60 * 60 }
     );
 
     resp.status(200).send({ token, username: user.username, name: user.name });
