dbf4c6a2f4518f4243707026ffda9f2fab70daa6
Jingwen Xia <amyfor7@gmail.com>
2024-09-23 21:16:11 +0800

    feat: sort blogs

--- Part5/bloglist-frontend/src/App.jsx
+++ Part5/bloglist-frontend/src/App.jsx
@@ -123,9 +123,11 @@ const App = () => {
       )}
 
       <h2>blogs</h2>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} update={updateBlog} />
-      ))}
+      {blogs
+        .sort((first, second) => second.likes - first.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} update={updateBlog} />
+        ))}
     </div>
   );
 };
