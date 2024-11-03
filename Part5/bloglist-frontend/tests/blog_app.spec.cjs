821c665252333091e51fd86140313e4fdaa3c0d5
Jingwen Xia <amyfor7@gmail.com>
2024-11-03 23:19:28 +0800

    playwright test: login success and failure

--- Part5/bloglist-frontend/tests/blog_app.spec.cjs
+++ Part5/bloglist-frontend/tests/blog_app.spec.cjs
@@ -2,7 +2,15 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Blog app', () => {
 
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing/reset');
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        username: 'cactus',
+        password: 'sekret'
+      }
+    });
+
     await page.goto('http://localhost:5173');
   });
 
@@ -18,4 +26,26 @@ describe('Blog app', () => {
 
     await expect(page.getByText('cactus logged-in')).toBeVisible();
   });
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('cactus');
+      await page.getByTestId('password').fill('sekret');
+      await page.getByRole('button', { name: 'login' }).click();
+
+      const hideWhenVisibleBlogs = await page.locator('.hideWhenVisible');
+      const blogText0 = await hideWhenVisibleBlogs.nth(0).innerText();
+      await expect(blogText0).toContain('What is hedgehock');
+      const blogText1 = await hideWhenVisibleBlogs.nth(1).innerText();
+      await expect(blogText1).toContain('What is xxx');
+    });
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('cactus');
+      await page.getByTestId('password').fill('wrongpassword');
+      await page.getByRole('button', { name: 'login' }).click();
+      await expect(page.getByText('wrong credentials')).toBeVisible();
+    });
+  });
+
 });
