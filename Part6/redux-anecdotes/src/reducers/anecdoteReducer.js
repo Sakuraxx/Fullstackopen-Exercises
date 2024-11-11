8db1b418c46832747efed8fc126fed906a9c5072
Jingwen Xia <amyfor7@gmail.com>
2024-11-11 17:16:02 +0800

    feat: implement the functionality for voting anecdotes

--- Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -17,13 +17,29 @@ const asObject = (anecdote) => {
   }
 }
 
+export const increateVotes = (id) => {
+  return {
+    type: 'VOTE',
+    payload: {id}
+  }
+}
+
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
-
-  return state
+  switch (action.type) {
+    case 'VOTE': {
+      const id = action.payload.id
+      return state.map(anecdote =>
+        anecdote.id === id
+        ? {...anecdote, votes: anecdote.votes + 1}
+        : anecdote
+      )
+    }
+    default: return state
+  }
 }
 
 export default reducer
