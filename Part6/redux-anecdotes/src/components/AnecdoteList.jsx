3d719b15604f86415a09796910556573915756e9
Jingwen Xia <amyfor7@gmail.com>
2024-11-17 16:22:56 +0800

    feat: add filter content functionality

--- Part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ Part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,14 @@ import { useSelector, useDispatch } from 'react-redux'
 import { increateVotes} from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(({anecdotes, filter}) => {
+      if(filter == '') {
+        return anecdotes;
+      }
+      console.log('!',anecdotes);
+      return anecdotes.filter(anecdote => anecdote.content.includes(filter));
+    })
+
     const dispatch = useDispatch()
 
     const vote = (id) => {
