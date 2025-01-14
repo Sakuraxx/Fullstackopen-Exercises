15c3d19f3ac0ba30affa08c77c9bed322ae7b023
Jingwen Xia <amyfor7@gmail.com>
2025-01-14 21:17:18 +0800

    format

--- Part7/redux-state-management/src/reducers/loginReducer.js
+++ Part7/redux-state-management/src/reducers/loginReducer.js
@@ -1,16 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const loginReducer = createSlice({
-    name: 'login',
-    initialState: null,
-    reducers: {
-        login(state, action) {
-            return action.payload;
-        },
-        logout(state, action) {
-            return null;
-        }
+  name: 'login',
+  initialState: null,
+  reducers: {
+    login(state, action) {
+      return action.payload;
+    },
+    logout(state, action) {
+      return null;
     }
+  }
 });
 
 export const { login, logout } = loginReducer.actions;
