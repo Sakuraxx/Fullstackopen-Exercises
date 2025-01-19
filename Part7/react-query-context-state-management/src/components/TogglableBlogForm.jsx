3be252ca98701e1f53212fbf21f38e4c38a5ea70
Jingwen Xia <amyfor7@gmail.com>
2025-01-19 21:40:01 +0800

    refactor: create blog using useMutation

--- Part7/react-query-context-state-management/src/components/TogglableBlogForm.jsx
+++ Part7/react-query-context-state-management/src/components/TogglableBlogForm.jsx
@@ -1,19 +1,40 @@
-import { createBlog } from '../reducers/blogReducer';
-import { useDispatch } from 'react-redux';
 import BlogForm from '../components/BlogForm';
 import Togglable from '../components/Togglable';
 import { useNotificationDispatch } from './NotificationContext';
+import blogService from '../services/blogService';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const TogglableBlogForm = ({ username }) => {
-  const dispatch = useDispatch();
   const notificationDispatch = useNotificationDispatch();
+
+  // create - useMutation
+  const queryClient = useQueryClient();
+  const newBlogsMutation = useMutation({
+    mutationFn: blogService.create,
+    onSuccess: (newBlog) => {
+      // refresh data
+      queryClient.invalidateQueries({ queryKey: ['blogs'] });
+
+      const blogs = queryClient.getQueryData('blogs');
+      queryClient.setQueryData(['blogs'], blogs.concat(newBlog));
+      console.log('after addition', blogs);
+
+      notificationDispatch({ type: 'NORMAL', payload: `Added blog '${newBlog.title}'` });
+      setTimeout(() => {notificationDispatch({ type: 'CLEAR' });}, 5000);
+    },
+    onError: (err) => {
+      notificationDispatch({ type: 'ERROR', payload: err.response.data.error });
+      setTimeout(() => {
+        notificationDispatch({ type: 'CLEAR', payload: '' });
+      }, 5000);
+    }
+  });
 
   const addBlog = async (nBlog) => {
     try {
       nBlog.author = username;
-      dispatch(createBlog(nBlog));
-      notificationDispatch({ type: 'NORMAL', payload: `Added blog '${nBlog.title}'` });
-      setTimeout(() => {notificationDispatch({ type: 'CLEAR' });}, 5000);
+      console.log('addBlog');
+      newBlogsMutation.mutate(nBlog);
     } catch (exception) {
       notificationDispatch({ type: 'NORMAL', payload: exception });
       setTimeout(() => {notificationDispatch({ type: 'CLEAR' });}, 5000);
