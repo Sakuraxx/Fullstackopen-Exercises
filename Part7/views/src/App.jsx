e782c596ec91a989006541a6be414038f426f7f7
Xia, Jing Wen <amyfor7@gmail.com>
2025-02-23 03:09:27 -0800

    feat: add navigation

--- Part7/views/src/App.jsx
+++ Part7/views/src/App.jsx
@@ -2,23 +2,34 @@ import BlogList from './components/BlogList';
 import Notification from './components/Notification';
 import UserDisplay from './components/UserDisplay';
 import {
-  Routes, Route
+  Routes, Route, Link
 } from 'react-router-dom'
 
 import {UserList, UserBlogList} from './components/Users'
 import BlogDetail from './components/BlogDetail';
 
 const App = () => {
+  const padding = {
+    paddingRight: 5
+  }
+
   return (
     <>
       <div>
+        <Link style={padding} to="/blogs">blogs</Link>
+        <Link style={padding} to="/users">users</Link>
+      </div>
+
+      <h2>Blogs App</h2>
+      <div>
         <Notification />
         <UserDisplay/>
-        <BlogList/>
+        {/* <BlogList/> */}
       </div>
 
       <Routes>
         <Route path="/users" element={<UserList/>} />
+        <Route path="/blogs" element={<BlogList/>} />
         <Route path="/users/:id" element={<UserBlogList/>} />
         <Route path="/blogs/:id" element={<BlogDetail/>}/>
       </Routes>
