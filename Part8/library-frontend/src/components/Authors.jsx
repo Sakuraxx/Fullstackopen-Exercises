c4c5dc4017b5bfca84d71845b2d62277a4f641fe
Xia, Jing Wen <amyfor7@gmail.com>
2025-03-02 00:58:44 -0800

    feat: update author birth year

--- Part8/library-frontend/src/components/Authors.jsx
+++ Part8/library-frontend/src/components/Authors.jsx
@@ -1,4 +1,6 @@
-import { gql, useQuery } from '@apollo/client'
+import { gql, useQuery, useMutation  } from '@apollo/client'
+import Select from 'react-select'
+import { useState } from "react";
 
 const ALL_AUTHORS = gql`
 query {
@@ -9,13 +11,28 @@ query {
   }
 }`
 
+const Edit_AUTHOR = gql`
+mutation editAuthor($name: String!, $birth: Int!) {
+    editAuthor(name: $name, setBornTo: $birth) {
+      name
+      born
+    }
+  }
+`
+
 const Authors = (props) => {
   if (!props.show) {
     return null
   }
 
-  const result = useQuery(ALL_AUTHORS)
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [birth, setBirth] = useState('')
   
+  const result = useQuery(ALL_AUTHORS)
+  const [ editAuthor ] = useMutation(Edit_AUTHOR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
+
   console.log('author', result)
 
   if (result.loading) {
@@ -23,6 +40,19 @@ const Authors = (props) => {
   }
 
   const authors = result.data.allAuthor
+  const options = authors.map(author => ({
+    value: author.name,
+    label: author.name
+  }));
+
+
+  const updateBirthYear = async (event) => {
+    event.preventDefault()
+
+    editAuthor({ variables: { name: selectedOption.value, birth: parseInt(birth) } })
+
+    setBirth('')
+  }
 
   return (
     <div>
@@ -43,6 +73,21 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+
+      <h2>Set birthyear</h2>
+      <form onSubmit={updateBirthYear}>  
+        <div>
+          Name:<Select options={options} onChange={setSelectedOption}/>
+        </div>
+        <div>
+            Born:
+            <input
+              value={birth}
+              onChange={({ target }) => setBirth(target.value)}
+            />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   )
 }
