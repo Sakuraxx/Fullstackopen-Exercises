33babed2cf9532e868e83c8ebcafed0853896f16
cactus <amyfor7@gmail.com>
2025-04-26 03:11:37 -0700

    feat: enhance BMI calculation script by adding argument parsing for height and weight; improve error handling

--- Part9 - TypeScript/BMI/calculateBmi.ts
+++ Part9 - TypeScript/BMI/calculateBmi.ts
@@ -1,6 +1,6 @@
 type BMIResult = 'Underweight' | 'Normal range' | 'Overweight' | 'Obese';
 
-const calculateBmi = (height_cm: number, weight_kg: number) : BMIResult =>
+const calculateBmi = (height_cm: number, weight_kg: number): BMIResult =>
 {
     if (height_cm <= 0 || weight_kg <= 0) {
         throw new Error('Height and weight must be positive values.');
@@ -21,17 +21,35 @@ const calculateBmi = (height_cm: number, weight_kg: number) : BMIResult =>
     }
 }
 
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments. Usage: ts-node calculateBmi.ts <height_cm> <weight_kg>');
+  if (args.length > 4) throw new Error('Too many arguments. Usage: ts-node calculateBmi.ts <height_cm> <weight_kg>');
+
+  const height = Number(args[2]);
+  const weight = Number(args[3]);
+
+  if (isNaN(height) || isNaN(weight)) {
+    throw new Error('Provided values were not numbers!');
+  }
+
+  return {
+    height: height,
+    weight: weight
+  };
+};
 
 try {
-    console.log(calculateBmi(180, 74)); 
-    console.log(calculateBmi(180, 50));
-    console.log(calculateBmi(180, 90)); 
-    console.log(calculateBmi(180, 110));
-    console.log(calculateBmi(180, -10));
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
 } catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    console.error(errorMessage);
+  let errorMessage = 'Something went wrong.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.error(errorMessage);
 }
