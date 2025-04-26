67de8252f3b6585182ee118315b990bb97470660
cactus <amyfor7@gmail.com>
2025-04-26 03:03:59 -0700

    feat: add exercise calculation functionality in TypeScript; include script to run calculation in package.json

---
+++ Part9 - TypeScript/BMI/calculateExercises.ts
@@ -0,0 +1,23 @@
+interface ExerciseRes {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+const calculateExercises = (dailyExerciseHour: Array<number>) : ExerciseRes => {
+    return { 
+        periodLength: 7,
+        trainingDays: 5,
+        success: false,
+        rating: 2,
+        ratingDescription: 'not too bad but could be better',
+        target: 2,
+        average: 1.9285714285714286
+      };
+};
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1]));
