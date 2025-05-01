9de008618130703e9f2da57cf0b44043da10069f
Xia, Jing Wen <amyfor7@gmail.com>
2025-05-01 07:18:25 -0800

    feat: init Patientor frontend

--- Part9 - TypeScript/Patientor - Backend/build/index.js
+++ Part9 - TypeScript/Patientor - Backend/build/index.js
@@ -4,10 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+// For all requests
+// app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+};
+app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
-const PORT = 3000;
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
